75c8897d05984560b1006eab169080a4c7278fc6
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2021-07-21 01:06:12 +0300

    Create Modal.jsx

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,71 @@
+import { Component } from 'react';
+import { ModalWindow, Overlay, Button } from './Modal.styles';
+import { IoClose } from 'react-icons/io5';
+import { Spiner } from '../Spiner/Spiner';
+import PropTypes from 'prop-types';
+
+export class Modal extends Component {
+  static propTypes = {
+    onModalClose: PropTypes.func.isRequired,
+    selectedImage: PropTypes.objectOf(PropTypes.string),
+  };
+
+  state = {
+    isImageLoaded: false,
+  };
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleOverlayClick = e => {
+    const { onModalClose } = this.props;
+
+    if (e.target !== e.currentTarget) {
+      return;
+    }
+
+    onModalClose();
+  };
+
+  handleKeyDown = e => {
+    const { onModalClose } = this.props;
+
+    if (e.code === 'Escape') {
+      onModalClose();
+    }
+  };
+
+  onImageLoadet = () => {
+    this.setState({
+      isImageLoaded: true,
+    });
+  };
+
+  render() {
+    const { onModalClose, selectedImage } = this.props;
+    const isImageLoaded = this.state.isImageLoaded;
+
+    return (
+      <Overlay onClick={this.handleOverlayClick}>
+        {!isImageLoaded && <Spiner />}
+        <ModalWindow>
+          <img
+            src={selectedImage.src}
+            alt={selectedImage.alt}
+            onLoad={this.onImageLoadet}
+          />
+          {isImageLoaded && (
+            <Button onClick={onModalClose}>
+              <IoClose size="30" />
+            </Button>
+          )}
+        </ModalWindow>
+      </Overlay>
+    );
+  }
+}
